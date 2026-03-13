4d2b4f68f57dd18e8e8e8056465a4d23b24e6b14
Mateusz <mkielbasa89@gmail.com>
2026-03-13 10:48:41 +0100

    ui: enable global mobile navigation by adding MobileNav to App root

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,12 +1,14 @@
 import {Component, signal} from '@angular/core';
 import {NavbarComponent} from './core/layout/navbar/navbar.component';
 import {RouterOutlet} from '@angular/router';
+import {MobileNavComponent} from './core/layout/mobile-nav.component/mobile-nav.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     NavbarComponent,
-    RouterOutlet
+    RouterOutlet,
+    MobileNavComponent,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
