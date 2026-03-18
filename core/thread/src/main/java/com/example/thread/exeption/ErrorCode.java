f122a8ed00bc5db426d6555b3a323faf09480438
Mateusz <mkielbasa89@gmail.com>
2026-03-18 20:51:04 +0100

    feat(thread-service): initialize base Spring Boot project

---
+++ core/thread/src/main/java/com/example/thread/exeption/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.example.thread.exeption;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    THREAD_NOT_FOUND("THREAD_001", "Thread not found", HttpStatus.NOT_FOUND),
+    THREAD_CREATION_FAILED("THREAD_002", "Could not create thread", HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED_ACCESS("THREAD_003", "You are not authorized to perform this action", HttpStatus.FORBIDDEN),
+
+    AUTHOR_NOT_FOUND_IN_CACHE("AUTHOR_001", "Author data is not yet synchronized", HttpStatus.SERVICE_UNAVAILABLE),
+    USER_NOT_FOUND("USER_001", "User not found", HttpStatus.NOT_FOUND);
+
+    private final String code;
+    private final String message;
+    private final HttpStatus httpStatus;
+}
