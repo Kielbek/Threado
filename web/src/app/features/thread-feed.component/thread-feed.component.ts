1733e5f1cdfd25d55f0cbe9780a0271093216d6c
Mateusz <mkielbasa89@gmail.com>
2026-03-19 11:33:01 +0100

    feat(feed): implement infinite scroll and skeleton loader for threads

---
+++ web/src/app/features/thread-feed.component/thread-feed.component.ts
@@ -0,0 +1,103 @@
+import {
+  Component,
+  DestroyRef,
+  effect,
+  ElementRef,
+  inject,
+  input,
+  OnInit,
+  signal,
+  untracked,
+  viewChild
+} from '@angular/core';
+import { finalize, Observable } from "rxjs";
+import { Page } from "../../core/model/page";
+import {ThreadSkeletonComponent} from '../thread-skeleton.component/thread-skeleton.component';
+import {ThreadComponent} from '../thread.component/thread.component';
+
+
+@Component({
+  selector: 'app-thread-feed',
+  imports: [
+    ThreadSkeletonComponent,
+    ThreadComponent
+  ],
+  templateUrl: './thread-feed.component.html',
+  styleUrl: './thread-feed.component.css',
+})
+export class ThreadFeedComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
+  readonly fetchStrategy = input.required<(page: number, size: number) => Observable<Page<any>>>();
+
+  readonly scrollAnchor = viewChild<ElementRef>('scrollAnchor');
+
+  threads = signal<any[]>([]);
+  currentPage = signal(0);
+  readonly pageSize = 10;
+
+  isLoading = signal(false);
+  isLastPage = signal(false);
+  isError = signal(false);
+
+  private observer: IntersectionObserver | null = null;
+
+  constructor() {
+    effect(() => {
+      this.fetchStrategy();
+      untracked(() => this.resetFeed());
+    });
+
+    this.destroyRef.onDestroy(() => {
+      this.observer?.disconnect();
+    });
+  }
+
+  ngOnInit() {
+    this.setupIntersectionObserver();
+  }
+
+  private resetFeed() {
+    this.threads.set([]);
+    this.currentPage.set(0);
+    this.isLastPage.set(false);
+    this.isError.set(false);
+    this.loadMore();
+  }
+
+  loadMore() {
+    if (this.isLoading() || this.isLastPage()) return;
+
+    this.isLoading.set(true);
+    this.isError.set(false);
+
+    this.fetchStrategy()(this.currentPage(), this.pageSize)
+      .pipe(
+        finalize(() => this.isLoading.set(false))
+      )
+      .subscribe({
+        next: (response) => {
+          this.threads.update(current => [...current, ...response.content]);
+          this.isLastPage.set(response.last);
+          this.currentPage.update(page => page + 1);
+        },
+        error: (err) => {
+          console.error('Błąd pobierania postów:', err);
+          this.isError.set(true);
+        }
+      });
+  }
+
+  private setupIntersectionObserver() {
+    const anchorElement = this.scrollAnchor()?.nativeElement;
+    if (!anchorElement) return;
+
+    this.observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        this.loadMore();
+      }
+    }, {rootMargin: '200px'});
+
+    this.observer.observe(anchorElement);
+  }
+}
